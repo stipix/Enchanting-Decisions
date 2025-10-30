daa66cf2c2c467a61fb8b2ed2cbd7f361e445819
Gronk <tntbarracuda@gmail.com>
2025-10-30 20:02:46 +0300

    Updated some enchantment mechanical workings, implemented custom enchantability stats for armor materials

---
+++ src/main/java/stipix/enchanting_decisions/mixin/ArmorMaterialMixin.java
@@ -0,0 +1,39 @@
+package stipix.enchanting_decisions.mixin;
+
+import net.minecraft.item.equipment.ArmorMaterial;
+import net.minecraft.item.equipment.ArmorMaterials;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(ArmorMaterial.class)
+public class ArmorMaterialMixin {
+
+    @Inject(method = "enchantmentValue", at = @At("HEAD"), cancellable = true)
+    private void modifiedEnchantability(CallbackInfoReturnable<Integer> cir) {
+        ArmorMaterial material = (ArmorMaterial)(Object)this;
+        if (material == ArmorMaterials.LEATHER) {
+            cir.setReturnValue(32);
+        }
+        else if (material == ArmorMaterials.CHAIN) {
+            cir.setReturnValue(32);
+        }
+        else if (material == ArmorMaterials.IRON) {
+            cir.setReturnValue(28);
+        }
+        else if (material == ArmorMaterials.DIAMOND) {
+            cir.setReturnValue(28);
+        }
+        else if (material == ArmorMaterials.GOLD) {
+            cir.setReturnValue(40);
+        }
+        else if (material == ArmorMaterials.NETHERITE) {
+            cir.setReturnValue(24);
+        }
+        else if (material == ArmorMaterials.ARMADILLO_SCUTE) {
+            cir.setReturnValue(24);
+        }
+    }
+
+}
---
+++ src/main/java/stipix/enchanting_decisions/mixin/TridentItemMixin.java
@@ -0,0 +1,114 @@
+package stipix.enchanting_decisions.mixin;
+
+import net.minecraft.component.EnchantmentEffectComponentTypes;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.MovementType;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.projectile.PersistentProjectileEntity;
+import net.minecraft.entity.projectile.ProjectileEntity;
+import net.minecraft.entity.projectile.TridentEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ProjectileItem;
+import net.minecraft.item.TridentItem;
+import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.stat.Stats;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(TridentItem.class)
+public abstract class TridentItemMixin extends Item implements ProjectileItem {
+
+    public TridentItemMixin(Item.Settings settings) {
+        super(settings);
+    }
+
+    @Inject(method = "use", at = @At("HEAD"), cancellable = true)
+    public void use(World world, PlayerEntity user, Hand hand, CallbackInfoReturnable<ActionResult> cir) {
+        ItemStack itemStack = user.getStackInHand(hand);
+        if (itemStack.willBreakNextUse()) {
+            cir.setReturnValue(ActionResult.FAIL);
+        } else {
+            user.setCurrentHand(hand);
+            cir.setReturnValue(ActionResult.CONSUME);
+        }
+        cir.cancel();
+    }
+
+    @Shadow
+    public abstract int getMaxUseTime(ItemStack stack, LivingEntity user);
+
+    @Inject(method = "onStoppedUsing", at = @At("HEAD"),cancellable = true)
+    public void onStoppedUsing(ItemStack stack, World world, LivingEntity user, int remainingUseTicks, CallbackInfoReturnable<Boolean> cir) {
+        if (user instanceof PlayerEntity playerEntity) {
+            int i = this.getMaxUseTime(stack, user) - remainingUseTicks;
+            if (i < 10) {
+                cir.setReturnValue(false);
+                cir.cancel();
+            } else {
+                float f = EnchantmentHelper.getTridentSpinAttackStrength(stack, playerEntity);
+                if (stack.willBreakNextUse()) {
+                    cir.setReturnValue(false);
+                    cir.cancel();
+                } else {
+                    RegistryEntry<SoundEvent> registryEntry = (RegistryEntry<SoundEvent>)EnchantmentHelper.getEffect(stack, EnchantmentEffectComponentTypes.TRIDENT_SOUND)
+                            .orElse(SoundEvents.ITEM_TRIDENT_THROW);
+                    playerEntity.incrementStat(Stats.USED.getOrCreateStat(this));
+                    if (f > 0.0F && playerEntity.isTouchingWaterOrRain()) {
+                        float g = playerEntity.getYaw();
+                        float h = playerEntity.getPitch();
+                        float j = -MathHelper.sin(g * (float) (Math.PI / 180.0)) * MathHelper.cos(h * (float) (Math.PI / 180.0));
+                        float k = -MathHelper.sin(h * (float) (Math.PI / 180.0));
+                        float l = MathHelper.cos(g * (float) (Math.PI / 180.0)) * MathHelper.cos(h * (float) (Math.PI / 180.0));
+                        float m = MathHelper.sqrt(j * j + k * k + l * l);
+                        j *= f / m;
+                        k *= f / m;
+                        l *= f / m;
+                        playerEntity.addVelocity(j, k, l);
+                        playerEntity.useRiptide(20, 8.0F, stack);
+                        if (playerEntity.isOnGround()) {
+                            float n = 1.1999999F;
+                            playerEntity.move(MovementType.SELF, new Vec3d(0.0, n, 0.0));
+                        }
+
+                        world.playSoundFromEntity(null, playerEntity, registryEntry.value(), SoundCategory.PLAYERS, 1.0F, 1.0F);
+                        cir.setReturnValue(true);
+                        cir.cancel();
+                    } else {
+                        if (world instanceof ServerWorld serverWorld) {
+                            stack.damage(1, playerEntity);
+                                ItemStack itemStack = stack.splitUnlessCreative(1, playerEntity);
+                                TridentEntity tridentEntity = ProjectileEntity.spawnWithVelocity(TridentEntity::new, serverWorld, itemStack, playerEntity, 0.0F, 2.5F, 1.0F);
+                                if (playerEntity.isInCreativeMode()) {
+                                    tridentEntity.pickupType = PersistentProjectileEntity.PickupPermission.CREATIVE_ONLY;
+                                }
+
+                                world.playSoundFromEntity(null, tridentEntity, registryEntry.value(), SoundCategory.PLAYERS, 1.0F, 1.0F);
+                                cir.setReturnValue(true);
+                                cir.cancel();
+                        }
+                    }
+
+                }
+            }
+        } else {
+            cir.setReturnValue(false);
+            cir.cancel();
+        }
+    }
+
+
+}
